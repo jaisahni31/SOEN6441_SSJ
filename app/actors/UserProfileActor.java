88afc39ac4e2edfa4a9459c10cc410addc3fb8f7
Sumit Ramesh Bhiungade <90669741+SumitRBca@users.noreply.github.com>
2021-12-05 23:28:03 -0500

    Add files via upload

---
+++ app/actors/UserProfileActor.java
@@ -0,0 +1,81 @@
+package actors;
+
+import Reddit.RedditHelper;
+import akka.actor.AbstractActorWithTimers;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Creates actor system which fetches the User Profile Info from PushShift API
+ * and notify the client of the response
+ * @author Sumit Ramesh Bhiungade
+ */
+public class UserProfileActor extends AbstractActorWithTimers {
+    private Map<String, ActorRef> actorsList = new LinkedHashMap<>();
+    private RedditHelper helper;
+
+
+    /**
+     * Constructor of the given Class
+     * @param helper PushShift api helper instance
+     */
+    public UserProfileActor(RedditHelper helper) {
+        this.helper = helper;
+    }
+
+    /**
+     * this method is used to create the instantiate a new Props
+     * @returns Props
+     */
+    public static Props props(RedditHelper instance) {
+        return Props.create(UserProfileActor.class, instance);
+    }
+
+
+
+    private static final class Tick {}
+
+    /**
+     * this class is used to register messaging instance for actor
+     * @author Sumit Ramesh Bhiungade
+     */
+    public static class RegisterMsg {
+        private final String keyword;
+        public RegisterMsg(String keyword) {
+            this.keyword = keyword;
+        }
+    }
+
+    /**
+     * Notifies clients about updates to the User Profile
+     * @author Sumit Ramesh Bhiungade
+     */
+
+    private void notifyClients() {
+        actorsList.forEach((key, instance) -> helper
+                .getUserPosts(key)
+                .thenApply((list) -> {
+                    UserActor.Response data = new UserActor.Response(list, "userProfile", key);
+                    instance.tell(data, self());
+                    return list;
+                }));
+    }
+
+    /**
+     * Action used to receive and manage the response with Actor class
+     * @author Sumit Ramesh Bhiungade
+     * @return Receive
+     */
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(UserProfileActor.RegisterMsg.class, register -> actorsList.put(register.keyword, sender()))
+                .match(UserProfileActor.Tick.class, obj -> notifyClients())
+                .matchEquals("stopActors", s -> System.out.println("got stop sign"))
+                .build();
+    }
+}
